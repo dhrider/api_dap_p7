710b188086d06213867be7ad9afccc1b0758f778
Hetphil <p_bordmann@orange.fr>
2020-08-26 09:28:26 +0200

    - Pull request changes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,18 +6,18 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
     /**
      * @Route("/api/products", name="products_list", methods={"GET"})
      * @param ProductRepository $productRepository
-     * @param SerializerInterface $serializer
      * @return Response
      */
-    public function products(ProductRepository $productRepository, SerializerInterface $serializer)
+    public function products(ProductRepository $productRepository)
     {
         return $this->json($productRepository->findAll(), 200, []);
     }
+
+
 }
